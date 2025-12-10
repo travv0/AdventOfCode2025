811031dc44b77fbcc971d933728deaec9f453402
Travis <tssund93@gmail.com>
2025-12-09 21:34:11 -0600

    day 8 part 1

--- day08/app/Main.hs
+++ day08/app/Main.hs
@@ -1,29 +1,69 @@
 module Main where
 
-import Data.Maybe (catMaybes)
+import Data.Maybe (mapMaybe)
+import Data.List (sortOn, findIndex, sortBy)
 import Data.List.Split (splitOn)
 import Data.Set (Set)
 import qualified Data.Set as S
 import Text.Read (readMaybe)
+import Data.Ord (comparing, Down (Down))
 
-data Box = Box Double Double Double deriving (Eq, Show)
+data Box = Box Double Double Double deriving (Eq, Ord, Show)
 type Circuit = Set Box
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  boxes <- parseInput <$> readFile "input.txt"
+  putStrLn $ "Part 1: " <> show (product $ take 3 $ sortBy (comparing Down) (S.size <$> buildCircuits boxes 1000))
 
 calcDistances :: [Box] -> [(Box, Box, Double)]
 calcDistances boxes = [(box1, box2, dist) | box1 <- boxes
                                           , box2 <- boxes
                                           , let dist = calcDistance box1 box2
-                                          , box1 /= box2]
+                                          , box1 < box2]
 
 calcDistance :: Box -> Box -> Double
 calcDistance (Box x1 y1 z1) (Box x2 y2 z2) =
   sqrt $ (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
 
+buildCircuits :: [Box] -> Int -> [Circuit]
+buildCircuits boxes = go [] (sortOn (\(_, _, d) -> d) $ calcDistances boxes)
+  where
+    go :: [Circuit] -> [(Box, Box, Double)] -> Int -> [Circuit]
+    go circuits _ 0 = circuits
+    go circuits ((box1, box2, _) : distances) n =
+      case (findIndex (S.member box1) circuits, findIndex (S.member box2) circuits) of
+        (Just i1, Just i2) ->
+          if i1 == i2
+             then go circuits distances (n - 1)
+             else go (mergeCircuits i1 i2 circuits) distances (n - 1)
+        (Just i1, Nothing) ->
+          go (updateCircuit i1 box1 box2 circuits) distances (n - 1)
+        (Nothing, Just i2) ->
+          go (updateCircuit i2 box1 box2 circuits) distances (n - 1)
+        _ -> go (S.fromList [box1, box2] : circuits) distances (n - 1)
+    go circuits _ _ = error $ show circuits
+
+
+updateCircuit :: Int -> Box -> Box -> [Circuit] -> [Circuit]
+updateCircuit i box1 box2 circuits =
+  case splitAt i circuits of
+    (headCircuits, circuit : tailCircuits) ->
+      headCircuits ++ (S.insert box1 (S.insert box2 circuit) : tailCircuits)
+    _ -> circuits
+
+mergeCircuits :: Int -> Int -> [Circuit] -> [Circuit]
+mergeCircuits i1 i2 circuits =
+  case splitAt (min i1 i2) circuits of
+    (headCircuits, circuit1 : restCircuits) ->
+      case splitAt (abs (i1 - i2) - 1) restCircuits of
+        (midCircuits, circuit2 : tailCircuits) ->
+          headCircuits ++ midCircuits ++ (S.union circuit1 circuit2 : tailCircuits)
+        a -> error $ "Bad mergeCircuits 2: " <> show a
+    a -> error $ "Bad mergeCircuits 1: " <> show a
+
 parseInput :: String -> [Box]
-parseInput = catMaybes . map parseLine . lines
+parseInput = mapMaybe parseLine . lines
 
 parseLine :: String -> Maybe Box
 parseLine s = case splitOn "," s of
