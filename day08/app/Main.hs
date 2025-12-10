c2386f700256ccb0aa43d1a52a122905643b5e92
Travis <tssund93@gmail.com>
2025-12-10 12:42:54 -0600

    day 8 part 2

--- day08/app/Main.hs
+++ day08/app/Main.hs
@@ -1,20 +1,26 @@
 module Main where
 
 import Data.Maybe (mapMaybe)
-import Data.List (sortOn, findIndex, sortBy)
+import Data.List (sortOn, findIndex)
 import Data.List.Split (splitOn)
 import Data.Set (Set)
 import qualified Data.Set as S
 import Text.Read (readMaybe)
-import Data.Ord (comparing, Down (Down))
+import Data.Ord (Down (Down))
 
-data Box = Box Double Double Double deriving (Eq, Ord, Show)
+data Box = Box Int Int Int deriving (Eq, Ord, Show)
 type Circuit = Set Box
 
 main :: IO ()
 main = do
   boxes <- parseInput <$> readFile "input.txt"
-  putStrLn $ "Part 1: " <> show (product $ take 3 $ sortBy (comparing Down) (S.size <$> buildCircuits boxes 1000))
+  let (circuits, _) = buildCircuits boxes 1000
+  putStrLn $
+    "Part 1: " <>
+      show (product $ take 3 $ sortOn Down (S.size <$> circuits))
+  let (_, finalProduct) = buildCircuits boxes (maxBound :: Int)
+  putStrLn $
+    "Part 2: " <> maybe "Failed to find final pair" show finalProduct
 
 calcDistances :: [Box] -> [(Box, Box, Double)]
 calcDistances boxes = [(box1, box2, dist) | box1 <- boxes
@@ -24,32 +30,55 @@ calcDistances boxes = [(box1, box2, dist) | box1 <- boxes
 
 calcDistance :: Box -> Box -> Double
 calcDistance (Box x1 y1 z1) (Box x2 y2 z2) =
-  sqrt $ (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
+  sqrt $ (fromIntegral x1 - fromIntegral x2) ** 2
+       + (fromIntegral y1 - fromIntegral y2) ** 2
+       + (fromIntegral z1 - fromIntegral z2) ** 2
 
-buildCircuits :: [Box] -> Int -> [Circuit]
-buildCircuits boxes = go [] (sortOn (\(_, _, d) -> d) $ calcDistances boxes)
+buildCircuits :: [Box] -> Int -> ([Circuit], Maybe Int)
+buildCircuits boxes =
+  go [] (sortOn (\(_, _, d) -> d) $ calcDistances boxes) Nothing
   where
-    go :: [Circuit] -> [(Box, Box, Double)] -> Int -> [Circuit]
-    go circuits _ 0 = circuits
-    go circuits ((box1, box2, _) : distances) n =
-      case (findIndex (S.member box1) circuits, findIndex (S.member box2) circuits) of
+    go :: [Circuit]
+       -> [(Box, Box, Double)]
+       -> Maybe Int
+       -> Int
+       -> ([Circuit], Maybe Int)
+    go circuits _ finalProduct 0 = (circuits, finalProduct)
+    go circuits
+       ((box1@(Box x1 _ _), box2@(Box x2 _ _), _) : distances)
+       finalProduct
+       n =
+      case (findIndex (S.member box1) circuits,
+            findIndex (S.member box2) circuits) of
         (Just i1, Just i2) ->
           if i1 == i2
-             then go circuits distances (n - 1)
-             else go (mergeCircuits i1 i2 circuits) distances (n - 1)
+             then go circuits distances finalProduct (n - 1)
+             else let mergedCircuits = (mergeCircuits i1 i2 circuits)
+                   in if length mergedCircuits == 1
+                         then go mergedCircuits distances (Just (x1 * x2)) 0
+                         else go mergedCircuits distances finalProduct (n - 1)
         (Just i1, Nothing) ->
-          go (updateCircuit i1 box1 box2 circuits) distances (n - 1)
+          go (updateCircuit i1 box2 circuits)
+             distances
+             finalProduct
+             (n - 1)
         (Nothing, Just i2) ->
-          go (updateCircuit i2 box1 box2 circuits) distances (n - 1)
-        _ -> go (S.fromList [box1, box2] : circuits) distances (n - 1)
-    go circuits _ _ = error $ show circuits
+          go (updateCircuit i2 box1 circuits)
+             distances
+             finalProduct
+             (n - 1)
+        _ -> go (S.fromList [box1, box2] : circuits)
+                distances
+                finalProduct
+                (n - 1)
+    go circuits _ _ _ = error $ show circuits
 
 
-updateCircuit :: Int -> Box -> Box -> [Circuit] -> [Circuit]
-updateCircuit i box1 box2 circuits =
+updateCircuit :: Int -> Box -> [Circuit] -> [Circuit]
+updateCircuit i box circuits =
   case splitAt i circuits of
     (headCircuits, circuit : tailCircuits) ->
-      headCircuits ++ (S.insert box1 (S.insert box2 circuit) : tailCircuits)
+      headCircuits ++ (S.insert box circuit : tailCircuits)
     _ -> circuits
 
 mergeCircuits :: Int -> Int -> [Circuit] -> [Circuit]
@@ -58,7 +87,9 @@ mergeCircuits i1 i2 circuits =
     (headCircuits, circuit1 : restCircuits) ->
       case splitAt (abs (i1 - i2) - 1) restCircuits of
         (midCircuits, circuit2 : tailCircuits) ->
-          headCircuits ++ midCircuits ++ (S.union circuit1 circuit2 : tailCircuits)
+          headCircuits
+            ++ midCircuits
+            ++ (S.union circuit1 circuit2 : tailCircuits)
         a -> error $ "Bad mergeCircuits 2: " <> show a
     a -> error $ "Bad mergeCircuits 1: " <> show a
 
@@ -73,26 +104,3 @@ parseLine s = case splitOn "," s of
                   z <- readMaybe zs
                   Just $ Box x y z
                 _ -> Nothing
-
-testBoxes :: [Box]
-testBoxes =
-  [ Box 162 817 812
-  , Box 57 618 57
-  , Box 906 360 560
-  , Box 592 479 940
-  , Box 352 342 300
-  , Box 466 668 158
-  , Box 542 29 236
-  , Box 431 825 988
-  , Box 739 650 466
-  , Box 52 470 668
-  , Box 216 146 977
-  , Box 819 987 18
-  , Box 117 168 530
-  , Box 805 96 715
-  , Box 346 949 466
-  , Box 970 615 88
-  , Box 941 993 340
-  , Box 862 61 35
-  , Box 984 92 344
-  , Box 425 690 689 ]
